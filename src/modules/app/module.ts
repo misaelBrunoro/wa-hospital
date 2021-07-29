5e27ac8edfa18c949d39bf429d182117ea03ec2d
Misael Brunoro <misaelbrunoroliberato@gmail.com>
2021-07-29 13:12:18 -0300

    import common

--- src/modules/app/module.ts
+++ src/modules/app/module.ts
@@ -1,10 +1,11 @@
 import { HttpModule, Module } from '@nestjs/common';
+import { CommonModule } from '../common';
 import { DatabaseModule } from '../database';
 import { ExamService } from './services/exam.service';
 import { LaboratoryService } from './services/laboratory.service';
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
+  imports: [HttpModule, CommonModule, DatabaseModule],
   controllers: [],
   providers: [ExamService, LaboratoryService],
 })
